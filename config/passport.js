e3b58206b88b788c5127599f6e0931c553d6cbf6
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    created authorization settings with local strategy

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const prisma = require("./db");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { username } });
+      if (!user) {
+        return done(null, false, { message: "user not found" });
+      }
+
+      await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
